83e2766cc29ffd7e907f3c1e6b1d32fd4c7f528b
Your Name <LJAndrea@D-69-91-152-106.dhcp4.washington.edu>
2017-04-13 14:10:23 -0700

    finished exercise 1, installed packages and analyzed vehicles data

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -7,27 +7,33 @@ install.packages('devtools')
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
+library(fueleconomy)
 
 # You should have have access to the `vehicles` data.frame
-
+all.vehicles <- data.frame(vehicles)
+View(vehicles)
 
 # Create a data.frame of vehicles from 1997
-
+vehicles.1997 <- all.vehicles[all.vehicles$year == 1997, ]
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique(vehicles.1997$year, incomparables = FALSE)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+two.wheel.drive <- all.vehicles[all.vehicles$drive == '2-Wheel Drive', ]
+two.wheel.20 <- two.wheel.drive[two.wheel.drive$cty > 20, ]
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
-
+worst.hwy <- two.wheel.20[two.wheel.20$hwy == min(two.wheel.20$hwy), 'id']
 
 # Write a function that takes a `year` and a `make` as parameters, and returns 
 # The vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
-
+MostMPG <- function(year, make) {
+  year.cars <- all.vehicles[all.vehicles$year == year, ]
+  car.make <- year.cars[year.cars$make == make, ]
+  return (car.make[car.make$hwy == max(car.make$hwy), 'model'])
+}
 
 # What was the most efficient honda model of 1995?
-
+MostMPG(1995, 'Honda')
 
